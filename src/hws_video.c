6698e9344c4dc249315070078fc27c234d0b45f6
Ben Hoff <hoff.benjamin.k@gmail.com>
2025-11-01 21:15:27 -0400

    update

--- src/hws_video.c
+++ src/hws_video.c
@@ -997,6 +997,7 @@ static const struct v4l2_ioctl_ops hws_ioctl_fops = {
 	.vidioc_querybuf = vb2_ioctl_querybuf,
 	.vidioc_qbuf = vb2_ioctl_qbuf,
 	.vidioc_dqbuf = vb2_ioctl_dqbuf,
+	.vidioc_expbuf = vb2_ioctl_expbuf,
 	.vidioc_streamon = vb2_ioctl_streamon,
 	.vidioc_streamoff = vb2_ioctl_streamoff,
 
